e4c123093a81c4d527787f8225d4da9d88f86f1a
Chetos26 <rlz.romero@yavirac.edu.ec>
2023-07-19 13:35:06 +0200

    .

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterHardwareComponent } from './register-hardware/register-hardware.component';
 import { UsersRegisterComponent } from './users-register/users-register.component';
+import { HardwareComponent } from './hardware/hardware.component';
+import { UsersComponent } from './users/users.component';
 
 
 
 @NgModule({
   declarations: [
+    HardwareComponent,
+    UsersComponent,
     RegisterHardwareComponent,
     UsersRegisterComponent
   ],
