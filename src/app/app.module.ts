313182ac2c87df69a510ece1cd1fa4a80a197604
Chetos26 <rlz.romero@yavirac.edu.ec>
2023-07-19 14:03:06 +0200

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AuthRoutingModule } from './auth/auth-routing.module';
 import { PagesRoutingModule } from './pages/pages-routing.module';
 import { AuthModule } from './auth/auth.module';
 import { PagesModule } from './pages/pages.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { PagesModule } from './pages/pages.module';
     PagesRoutingModule,
     AuthModule,
     PagesModule,
+    SharedModule,
     AppRoutingModule
   ],
   providers: [],
